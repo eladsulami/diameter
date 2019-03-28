a55681ead3f0c8d2b0c6be4564ea2b5ef826af8c
eladsulami <elad.sulami@worldmate.com>
2019-03-28 17:07:18 +0200

    update code

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ def distance(a, b):
 
 def main(draw_always, points, runs):
     for i in range(runs):
-        xs, ys = np.random.random(size=(2,points))
+        print("-------------------------------------")
+        xs, ys = np.random.randint(low=0, high=100, size=(2,points))
 
         diam = -inf
         alg_diam = -inf
@@ -34,6 +35,7 @@ def main(draw_always, points, runs):
             if alg_p2 is None:
                 alg_p2 = (x, y)
                 alg_diam = hypot(alg_p1[0] - x, alg_p1[1] - y)
+                print("update", alg_p1, alg_p2, alg_diam)
                 continue
 
             dist1 = distance(alg_p1, (x, y))
@@ -41,18 +43,22 @@ def main(draw_always, points, runs):
             if dist2 > alg_diam and dist2 > dist1:
                 alg_p1 = (x, y)
                 alg_diam = dist2
+                print("update", alg_p1, alg_p2, alg_diam)
                 continue
             if dist1 > alg_diam and dist1 >= dist2:
                 alg_p2 = (x, y)
                 alg_diam = dist1
+                print("update", alg_p1, alg_p2, alg_diam)
 
 
-        # print(real_p1, real_p2, diam)
-        # print(alg_p1, alg_p2, alg_diam)
-        print(alg_diam/diam, 1/sqrt(2))
+        print("real result", real_p1, real_p2, diam)
+        print("alg result", alg_p1, alg_p2, alg_diam)
+        print("ratio", alg_diam/diam, "1/sqrt(3)", 1/sqrt(3))
 
-        if alg_diam/diam < 1/sqrt(2) or draw_always:
+        if alg_diam/diam < 1/sqrt(3) or draw_always:
             import matplotlib.pyplot as plt
+            for z in zip(xs, ys):
+                print(z)
 
             plt.scatter(xs, ys)
             plt.plot([real_p1[0], real_p2[0]], [real_p1[1], real_p2[1]], 'ro-')
